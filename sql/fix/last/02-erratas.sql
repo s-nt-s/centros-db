5278708e1c3a39b646422e8df96f394a9962eca1
s-nt-s <santos82h@gmail.com>
2026-06-03 19:35:25 +0200

    correción de errores en las fuentes

--- sql/fix/last/02-erratas.sql
+++ sql/fix/last/02-erratas.sql
@@ -1,9 +1,9 @@
 UPDATE CENTRO SET jornada='C' where id in (
-    -- https://site.educa.madrid.org/cp.puertadelasierra.venturada/wp-content/uploads/cp.puertadelasierra.venturada/2025/03/VenturadaJE2526_CEIPS-Puerta-de-la-Sierra_50467727.pdf vía 
-    -- https://site.educa.madrid.org/cp.puertadelasierra.venturada/index.php/documentos-de-centro/
-    28073872,
-    -- https://www.educa2.madrid.org/web/centro.cp.navalafuente -> sección calendario escolar
-    28082174
+-- https://site.educa.madrid.org/cp.puertadelasierra.venturada/wp-content/uploads/cp.puertadelasierra.venturada/2025/03/VenturadaJE2526_CEIPS-Puerta-de-la-Sierra_50467727.pdf vía 
+-- https://site.educa.madrid.org/cp.puertadelasierra.venturada/index.php/documentos-de-centro/
+28073872,
+-- https://www.educa2.madrid.org/web/centro.cp.navalafuente -> sección calendario escolar
+28082174
 )
 ;
 INSERT OR IGNORE INTO QUERY_CENTRO (centro, query) values
